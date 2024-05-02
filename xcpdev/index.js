4915872743f7f2a95caa3af356016508e57c2568
Juan <dev@juanaleman.com>
2024-05-02 16:44:50 -0400

    most basic first commit for a (express based) xcp application

---
+++ xcpdev/index.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+const port = 3000;
+
+app.get('/', async (req, res) => {
+    res.status(200).json({
+        online: true
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
