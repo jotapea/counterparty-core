c0a9bdc8e22e911fc5614f8d3c82e0c64c31e916
Juan <dev@juanaleman.com>
2024-05-24 15:23:29 -0400

    apps with read access to the counterparty db - see https://github.com/CNTRPRTY/xcpdev-api for a full project example

--- apps/nodejs/index.js
+++ apps/nodejs/index.js
@@ -2,6 +2,12 @@
 const express = require('express');
 const cors = require('cors');
 
+// // read only
+// import sqlite3 from 'better-sqlite3';
+// const DB_PATH = '/data/counterparty/counterparty.db';
+// const db = sqlite3(DB_PATH, { readonly: true });
+// // ... (see: https://github.com/CNTRPRTY/xcpdev-api)
+
 const app = express();
 app.use(cors());
 const port = 3001;
